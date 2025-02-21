c1bc9e709b448611152dea225d11539140d55387
Eric Flumerfelt <eflumerf@fnal.gov>
2025-02-21 13:01:06 -0600

    Require the sequence ID argument for creating MetadataFragments

--- artdaq-core/Data/MetadataFragment.hh
+++ artdaq-core/Data/MetadataFragment.hh
@@ -16,11 +16,11 @@ public:
 
 	ArtdaqMetadata get_metadata();
 
-	static FragmentPtr CreateMetadataFragment(ArtdaqMetadata metadata, Fragment::type_t type, Fragment::sequence_id_t seqID = 0, Fragment::timestamp_t ts = 0, Fragment::fragment_id_t id = 0);
-	static FragmentPtr CreateEndOfSubrunFragment(int rank, Fragment::sequence_id_t seqID = 0, Fragment::timestamp_t ts = 0, Fragment::fragment_id_t id = 0) { return CreateMetadataFragment({rank}, Fragment::EndOfSubrunFragmentType, seqID, ts, id); }
-	static FragmentPtr CreateEndOfRunFragment(int rank, Fragment::sequence_id_t seqID = 0, Fragment::timestamp_t ts = 0, Fragment::fragment_id_t id = 0) { return CreateMetadataFragment({rank}, Fragment::EndOfRunFragmentType, seqID, ts, id); }
-	static FragmentPtr CreateStartOfSubrunFragment(int rank, Fragment::sequence_id_t seqID = 0, Fragment::timestamp_t ts = 0, Fragment::fragment_id_t id = 0) { return CreateMetadataFragment({rank}, Fragment::StartOfSubrunFragmentType, seqID, ts, id); }
-	static FragmentPtr CreateStartOfRunFragment(int rank, Fragment::sequence_id_t seqID = 0, Fragment::timestamp_t ts = 0, Fragment::fragment_id_t id = 0) { return CreateMetadataFragment({rank}, Fragment::StartOfRunFragmentType, seqID, ts, id); }
+	static FragmentPtr CreateMetadataFragment(ArtdaqMetadata metadata, Fragment::type_t type, Fragment::sequence_id_t seqID, Fragment::timestamp_t ts = 0, Fragment::fragment_id_t id = 0);
+	static FragmentPtr CreateEndOfSubrunFragment(int rank, Fragment::sequence_id_t seqID, Fragment::timestamp_t ts = 0, Fragment::fragment_id_t id = 0) { return CreateMetadataFragment({rank}, Fragment::EndOfSubrunFragmentType, seqID, ts, id); }
+	static FragmentPtr CreateEndOfRunFragment(int rank, Fragment::sequence_id_t seqID, Fragment::timestamp_t ts = 0, Fragment::fragment_id_t id = 0) { return CreateMetadataFragment({rank}, Fragment::EndOfRunFragmentType, seqID, ts, id); }
+	static FragmentPtr CreateStartOfSubrunFragment(int rank, Fragment::sequence_id_t seqID, Fragment::timestamp_t ts = 0, Fragment::fragment_id_t id = 0) { return CreateMetadataFragment({rank}, Fragment::StartOfSubrunFragmentType, seqID, ts, id); }
+	static FragmentPtr CreateStartOfRunFragment(int rank, Fragment::sequence_id_t seqID, Fragment::timestamp_t ts = 0, Fragment::fragment_id_t id = 0) { return CreateMetadataFragment({rank}, Fragment::StartOfRunFragmentType, seqID, ts, id); }
 
 private:
 	artdaq::Fragment const& artdaq_fragment_;
